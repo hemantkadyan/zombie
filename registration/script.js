76f02a25fe327d4fbf78aeb30cd457afe451c638
Vikram Waradpande <f2015454@pilani.bits-pilani.ac.in>
2017-03-15 18:12:03 +0530

    Added stylesheet and JS

---
+++ registration/script.js
@@ -0,0 +1,31 @@
+$('#player2').addClass("disabled");
+
+
+
+//Function after clicking submit of player1
+$('#submit1').on('click', function(e){
+   e.preventDefault();
+   
+   if(confirm("Is the information entered by you correct?")){
+// Add AJAX Call Here
+
+$('#player2').removeClass("disabled");
+$('#player1').addClass("disabled");
+
+ }
+
+});
+
+
+
+//Function after clicking submit of player2
+$('#submit2').on('click', function(e){
+   e.preventDefault();
+  
+  if(confirm("Is the information entered by you correct?")){
+  // Add AJAX Call Here
+
+  }
+
+
+});
