7e4715da3296ccb0cbd0e5a2c250ab8fe82ad811
Vikram Waradpande <f2015454@pilani.bits-pilani.ac.in>
2017-03-16 15:04:23 +0530

    Update script.js

--- registration/script.js
+++ registration/script.js
@@ -23,7 +23,9 @@ $('#submit2').on('click', function(e){
    e.preventDefault();
   
   if(confirm("Is the information entered by you correct?")){
-  // Add AJAX Call Here
+  e.preventDefault();
+  if(confirm("Is the information entered by you correct?")){
+  $('#form1').submit();
 
   }
 
