9337ac3cac9b198e4c893bcd41b2855544a43247
Vikram Waradpande <f2015454@pilani.bits-pilani.ac.in>
2017-03-18 19:58:56 +0530

    Update script.js
    Added form validation elements. Now name, college name and phone numbers will be validated.

--- registration/script.js
+++ registration/script.js
@@ -1,10 +1,20 @@
+var letters = /^[a-zA-Z ]+$/;          //Regex pattern to identify names
+var phone = /^[7-9]+[0-9]{9}$/;        //Regex pattern to identify phone number
+
 $('#player2').addClass("disabled");
-
-
 
 //Function after clicking submit of player1
 $('#submit1').on('click', function(e){
    e.preventDefault();
+   
+  var name1 = document.forms[0].elements[0].value;
+  var college_name1 = document.forms[0].elements[2].value;
+  var phone_number1 = document.forms[0].elements[3].value;
+   if(validateData(name1,phone_number1,college_name1))
+    {
+    }
+    else
+    {return;}
    
    if(confirm("Is the information entered by you correct?")){
 // Add AJAX Call Here
@@ -23,6 +33,15 @@ $('#submit2').on('click', function(e){
    
   
   e.preventDefault();
+   
+  var name2 = document.forms[0].elements[5].value;
+  var college_name2 = document.forms[0].elements[7].value;
+  var phone_number2 = document.forms[0].elements[8].value;
+  
+if(validateData(name2, phone_number2,college_name2))
+   {}
+   else{return;}
+   
   if(confirm("Is the information entered by you correct?")){
   $('#form1').submit();
 
@@ -30,3 +49,26 @@ $('#submit2').on('click', function(e){
 
 
 });
+
+function validateData(name,phone_number,college_name){
+	if(name.match(letters)&&college_name.match(letters))  
+    {  
+        
+    }  
+  else  
+     {  
+     alert("Check Name or College Name fields! Please enter ONLY letters in these fields");  
+     return false;
+     }
+
+    if(phone_number.match(phone))
+    {
+      return true;
+    }   
+    else
+    {
+      
+      alert("Enter a valid phone number!");
+      return false;
+    }
+}
